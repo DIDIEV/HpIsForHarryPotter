f5ff23a8988e7587c4d845c5a1fcaebbc3523888
DIDIEV <stiventorres608@gmail.com>
2025-09-03 17:38:00 -0500

    finished mages and spells

--- Main.java
+++ Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Mage harry = new Harry();
         Mage ron = new Ron();
+        Mage hermione = new Hermione();
 
         harry.performSpell(new ExpectoPatronum());
         harry.showSignatureSpell();
+
+        hermione.performSpell(new OculusReparo());
+        hermione.showSignatureSpell();
 
         ron.performSpell(new Expelliarmus());
         ron.showSignatureSpell();
